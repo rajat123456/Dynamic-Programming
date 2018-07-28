baac503439402e0e0a7c687edce88268c9ed0e0b
Rajat Rawat <rajatrawat1997@gmail.com>
2018-07-28 19:04:03 +0530

    Create Nth Fibonacci Number(Using Top Down Memoization DP).java

---
+++ Nth Fibonacci Number/Nth Fibonacci Number(Using Top Down Memoization DP).java
@@ -0,0 +1,28 @@
+// Nth Fibonacci Number(Using Top Down Memoization DP)
+
+import java.util.*;
+class Rextester
+{
+	public static void main(String[] args) {
+		
+        Scanner sc=new Scanner(System.in);
+     		int n=sc.nextInt();
+        int[]storage=new int[n+1];
+        System.out.println(Memoizedfib(n,storage));
+}
+
+static int Memoizedfib(int n, int[]storage)
+    {
+        if(storage[n]!=0)
+            return storage[n];
+        
+        if(n==0 || n==1)
+        {
+            storage[n]=n;
+            return storage[n];
+        }
+        
+        storage[n]=Memoizedfib(n-1,storage)+Memoizedfib(n-2,storage);
+        return storage[n];
+    }
+}
