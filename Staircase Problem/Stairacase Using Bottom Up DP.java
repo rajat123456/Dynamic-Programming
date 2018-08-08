689022b63a6ce117c0dd9f6a68a4f9f6d4abd918
Rajat Rawat <rajatrawat1997@gmail.com>
2018-08-08 20:58:51 +0530

    Update Stairacase Using Bottom Up DP.java

--- Staircase Problem/Stairacase Using Bottom Up DP.java
+++ Staircase Problem/Stairacase Using Bottom Up DP.java
@@ -6,33 +6,21 @@
 public class Solution {
 public static long staircase(int n){
 	
+     if(n==0 ||n==1)
+       return 1;
+  
+     if(n==2)
+       return 2;
+  
      long[]storage=new long[n+1];
+     storage[0]=1;
      storage[1]=1;
-     return stair(n,storage);
+     storage[2]=2;
+     
+     for(int q=3;q<=n;q++)
+     {
+         storage[q]=storage[q-1]+storage[q-2]+storage[q-3];
+     }
+  return storage[n];
   } 
-  static long stair(int n, long[]storage)
-  {
-    if(storage[n]==0)
-      
-    {
-      long count=0;
-      if(n==0)
-        return 1;
-      
-      if(n-1>=0)
-        count+=stair(n-1,storage);
-      
-      if(n-2>=0)
-        count+=stair(n-2,storage);
-      
-      if(n-3>=0)
-        count+=stair(n-3,storage);
-      
-      storage[n]=count;
-      return storage[n];
-    }
-    
-    else
-    return storage[n];
-  }
 }
