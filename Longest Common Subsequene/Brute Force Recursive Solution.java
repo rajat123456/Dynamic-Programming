4cec12c4b5e442c3ed99cf6a6fd3547f4a60cf4b
Rajat Rawat <rajatrawat1997@gmail.com>
2018-08-12 17:20:35 +0530

    Update Brute Force Recursive Solution.java

--- Longest Common Subsequene/Brute Force Recursive Solution.java
+++ Longest Common Subsequene/Brute Force Recursive Solution.java
@@ -17,15 +17,20 @@ public class Main
 	    if(first.length()==0 || second.length()==0)
 	    return output;
 	    
+	    //First Character of both string matches
 	    if(first.charAt(0)==second.charAt(0))
 	    output+=LCS(first.substring(1),second.substring(1),first.charAt(0)+"");
 	    
 	    else
 	    {
+		//If first character of 1st string is not part of LCS    
 	        String one=LCS(first.substring(1),second,output);
+		//If first character of 2nd string is not part of LCS    
 	        String sec=LCS(first,second.substring(1),output);
+		//If first character of both strings are not part of LCS    
 	        String thi=LCS(first.substring(1),second.substring(1),output);
 	        
+		//Returning the maximum length Substring out of above 3 found    
 	        if(one.length()>=sec.length() && one.length()>=thi.length())
 	        return one;
 	        
