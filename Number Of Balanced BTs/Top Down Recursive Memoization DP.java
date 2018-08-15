24b7e645f5535e3dd81496724cddaf9697237065
Rajat Rawat <rajatrawat1997@gmail.com>
2018-08-15 17:37:42 +0530

    Create Top Down Recursive Memoization DP.java

---
+++ Number Of Balanced BTs/Top Down Recursive Memoization DP.java
@@ -0,0 +1,26 @@
+public class Solution {
+
+   static int m=1000000007;	
+public static int balancedTreesOfHeightH(int height){
+
+      int storage[]=new int[height+1];
+      storage[0]=1;
+      storage[1]=1;
+      return ans(height,storage);
+	}
+  
+  static int ans(int height, int[]storage)
+  {
+        if(storage[height]!=0)
+        return storage[height];
+      
+      long x=ans(height-1,storage);
+      long y=ans(height-2,storage);
+      
+      int first=(int)(((long)(x)*x)%m);
+      int second=(int)(((long)(x)*2*y)%m);
+      
+      storage[height]= (int)(((long)(first) + second )%m);
+      return storage[height]; 
+  }
+}
